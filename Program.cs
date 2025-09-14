9dd65e31a13e60231395e858764bb57422059c80
Sijal44 <sajad.azizi92@yahoo.com>
2025-09-14 11:58:18 +0200

    Jag  har ändrat Tea.display() plats

--- Program.cs
+++ Program.cs
@@ -10,18 +10,20 @@ namespace Exersice
             family.Name = "Ericson";
             family.Year = 2020; 
 
-           Student student = new Student();    
+            Student student = new Student();    
             student.Name = "John";
 
             Teacher teacher = new Teacher();
             teacher.SchoolName = "Handelsakademin";
             teacher.Age = 35;
             teacher.StudentID = "S12345";
-            teacher.Display();
 
             Console.WriteLine($"Hello {student.Name}");
             Console.WriteLine($"Mr {family.Name} ,you are born in :{family.Year}");
             Console.WriteLine("Welcome to the school");
+
+            teacher.Display();
+
 
 
         }
